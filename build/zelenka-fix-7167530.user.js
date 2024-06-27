d392989dcd9dc84e2914b4ac2a3a0eb055b91c58
laytara <laytara@outlook.com>
2024-06-28 01:19:13 +0300

    Update zelenka-fix-7167530.user.js

--- build/zelenka-fix-7167530.user.js
+++ build/zelenka-fix-7167530.user.js
@@ -5,11 +5,11 @@
 // @author       Слава Соколов (equilpres)
 // @iconURL      https://www.google.com/s2/favicons?sz=128&domain=zelenka.guru
 // @match        *://lolz.live/*
-// @match        *://localhost/*
+// @match        *://pub-2c6eb843124a4b7c91c44760fe682184.r2.dev/*
 // @grant        none
 // @run-at       document-start
 // @noframes
-// @webRequest   [{"selector":"*://lolz.live/js/lolzteam/im/im_socket.js?_v=*","action":{"redirect":"http://localhost/v1/im_socket.js"}}]
+// @webRequest   [{"selector":"*://lolz.live/js/lolzteam/im/im_socket.js?_v=*","action":{"redirect":"https://pub-2c6eb843124a4b7c91c44760fe682184.r2.dev/v1/im_socket.js"}}]
 // ==/UserScript==
 
 /* eslint-disable */
