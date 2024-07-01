88c6ddef52ad69d66a08ecea63b40a166d77b316
laytara <laytara@outlook.com>
2024-07-01 05:35:22 +0300

    chore: sync changes

--- static/im_socket.js
+++ static/im_socket.js
@@ -1,4 +1,12 @@
 var __webpack_exports__ = {};
+function _array_like_to_array(arr, len) {
+	if (len == null || len > arr.length) len = arr.length;
+	for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+	return arr2;
+}
+function _array_with_holes(arr) {
+	if (Array.isArray(arr)) return arr;
+}
 function _define_property(obj, key, value) {
 	if (key in obj) {
 		Object.defineProperty(obj, key, {
@@ -11,6 +19,46 @@ function _define_property(obj, key, value) {
 		obj[key] = value;
 	}
 	return obj;
+}
+function _iterable_to_array_limit(arr, i) {
+	var _i = arr == null ? null : (typeof Symbol !== 'undefined' && arr[Symbol.iterator]) || arr['@@iterator'];
+	if (_i == null) return;
+	var _arr = [];
+	var _n = true;
+	var _d = false;
+	var _s, _e;
+	try {
+		for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) {
+			_arr.push(_s.value);
+			if (i && _arr.length === i) break;
+		}
+	} catch (err) {
+		_d = true;
+		_e = err;
+	} finally {
+		try {
+			if (!_n && _i['return'] != null) _i['return']();
+		} finally {
+			if (_d) throw _e;
+		}
+	}
+	return _arr;
+}
+function _non_iterable_rest() {
+	throw new TypeError(
+		'Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.',
+	);
+}
+function _sliced_to_array(arr, i) {
+	return _array_with_holes(arr) || _iterable_to_array_limit(arr, i) || _unsupported_iterable_to_array(arr, i) || _non_iterable_rest();
+}
+function _unsupported_iterable_to_array(o, minLen) {
+	if (!o) return;
+	if (typeof o === 'string') return _array_like_to_array(o, minLen);
+	var n = Object.prototype.toString.call(o).slice(8, -1);
+	if (n === 'Object' && o.constructor) n = o.constructor.name;
+	if (n === 'Map' || n === 'Set') return Array.from(n);
+	if (n === 'Arguments' || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _array_like_to_array(o, minLen);
 }
 var Im = Im || {};
 Im.navigationCache = Im.navigationCache || {};
@@ -132,7 +180,6 @@ function formatTime(time) {
 	Im.Start.prototype =
 		((_obj = {
 			__construct: function __construct($container) {
-				var _this = this;
 				this.spinnerHTML =
 					'<div class="spinner">				<div class="bounce1"></div>				<div class="bounce2"></div>				<div class="bounce3"></div>			</div>';
 				var CapImage = new Image();
@@ -1427,22 +1474,19 @@ function formatTime(time) {
 			if (!disableScrolling) {
 				var $messageList = this.$dialog.find('#ConversationMessageList');
 				if ($messageList.length) {
-					const [$message] = $messageList;
-
+					var _$messageList = _sliced_to_array($messageList, 1),
+						$message = _$messageList[0];
 					if (instant) {
 						if (!this.autoScrollEnabled || this.isSmoothScrolling) {
 							return;
 						}
-
 						$messageList.scrollTop($message.scrollHeight);
 					} else {
 						if (this.smoothScrollAnimation) {
 							this.$messageList.stop();
 							this.smoothScrollAnimation.stop();
 						}
-
 						this.isSmoothScrolling = true;
-
 						this.smoothScrollAnimation = $messageList.animate(
 							{
 								scrollTop: $message.scrollHeight,
